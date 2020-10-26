46442e3864f363bcead85fbe92c2e8cfd31383af
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2020-10-26 05:45:54 -0400

    Updated PS1 to reflect repo

--- TC-PM-01_02 deployment.ps1
+++ TC-PM-01_02 deployment.ps1
@@ -1,10 +1,10 @@
 $AZRG3 = "The_Taco_King_3"
 $AZRG4 = "The_Taco_King_4"
 $tags =@{"Type"="VMs"}
-$Template = "C:\Users\Benja\source\repos\Azrue-ARM\Win16template.json"
-$Param3 = "C:\Users\Benja\source\repos\Azrue-ARM\TC-PM-01.parameters.json"
-$Param4 = "C:\Users\Benja\source\repos\Azrue-ARM\TC-PM-02.parameters.json"
+$Template = "https://raw.githubusercontent.com/benjaminsmorgan/Azrue-ARM/main/Win16template.json"
+$Param3 = "https://raw.githubusercontent.com/benjaminsmorgan/Azrue-ARM/main/TC-PM-01.parameters.json"
+$Param4 = "https://raw.githubusercontent.com/benjaminsmorgan/Azrue-ARM/main/TC-PM-02.parameters.json"
 New-AzResourceGroup -Name $AZRG3 -Location 'eastus' -Tag $tags 
 New-AzResourceGroup -Name $AZRG4 -Location 'eastus' -Tag $tags
-New-AzResourceGroupDeployment -ResourceGroupName $AZRG3 -TemplateFile $Template -TemplateParameterFile $Param3
-New-AzResourceGroupDeployment -ResourceGroupName $AZRG3 -TemplateFile $Template -TemplateParameterFile $Param4
+New-AzResourceGroupDeployment -ResourceGroupName $AZRG3 -TemplateUri $Template -TemplateParameterUri $Param3
+New-AzResourceGroupDeployment -ResourceGroupName $AZRG3 -TemplateUri $Template -TemplateParameterUri $Param4
