317babbddacecfea5d91f08dfc8e02a4ada35544
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2020-11-01 07:43:28 -0500

    Major Updates
    Adds Resource lock removal
    Adds partial help function

--- TC-PM-Training Deployment.ps1
+++ TC-PM-Training Deployment.ps1
@@ -1,7 +1,6 @@
 #Written by Benjamin Morgan benjamin.s.morgan@outlook.com with asstance from https://github.com/Azure
 #Code written for training purposes AZ-104 exam. Not for commercial use!
-function Training-Deploy-WinVM #Deploys up to 4 VMs into 4 Azure RGs using templates stored in https://github.com/benjaminsmorgan/Azrue-ARM
-{
+function Training-Deploy-WinVM { #Deploys up to 4 VMs into 4 Azure RGs using templates stored in https://github.com/benjaminsmorgan/Azrue-ARM Resources are deployed to EastUS
     [CmdletBinding()]
     param 
     (
@@ -27,8 +26,8 @@ function Training-Deploy-WinVM #Deploys up to 4 VMs into 4 Azure RGs using templ
     ## End of $var creation
     ## Being resource and resource group deployment
         if (!$AZRG1) { # Checks for first parameter and ends script ff no parameters provided
-            Write-Host "Please Provide Upto 4 Resource Group Names for Deployment"
-            Write-Host "Example: Deploy-TrainingEn TacoGroup1 TacoGroup2 TacoGroup3 TacoGroup4 "
+            Write-Host "Please Provide Up To 4 Resource Group Names for Deployment"
+            Write-Host "Example: Training-Deploy-WinVM TacoGroup1 TacoGroup2 TacoGroup3 TacoGroup4 "
             Break # Terminates Script
         }       
             else {
@@ -37,7 +36,7 @@ function Training-Deploy-WinVM #Deploys up to 4 VMs into 4 Azure RGs using templ
                 #Write-Host $AZRG1 # This line is used for user input validation testing for If Else statements. Add # to the 2 line above and remove # from beginning of this line
             }
         if (!$AZRG2) {
-            Write-Host "No Additional Prarameters Provided, Ending Deployment"
+            Write-Host "No Additional Parameters Provided, Ending Deployment"
             Break
         }
             else {
@@ -46,7 +45,7 @@ function Training-Deploy-WinVM #Deploys up to 4 VMs into 4 Azure RGs using templ
                 #Write-Host $AZRG1 # This line is used for user input validation testing for If Else statements. Add # to the 2 line above and remove # from beginning of this line
             }
         if (!$AZRG3) {
-            Write-Host "No Additional Prarameters Provided, Ending Deployment"
+            Write-Host "No Additional Parameters Provided, Ending Deployment"
             Break
         }
             else {
@@ -55,7 +54,7 @@ function Training-Deploy-WinVM #Deploys up to 4 VMs into 4 Azure RGs using templ
                 #Write-Host $AZRG1 # This line is used for user input validation testing for If Else statements. Add # to the 2 line above and remove # from beginning of this line
             }
         if (!$AZRG4) {
-            Write-Host "No Additional Prarameters Provided, Ending Deployment"
+            Write-Host "No Additional Parameters Provided, Ending Deployment"
             Break
         }
             else {
@@ -92,7 +91,7 @@ function Training-connect-RDC { # Connects upto 4 VMs based of resource group na
         # End first parameter run
         # Begin additional parameter runs if present
         if (!$AZRG2) { # Check for additional parameters
-            Write-Host "No Additional Prarameters Provided, Ending Script"
+            Write-Host "No Additional Parameters Provided, Ending Script"
             Break # Terminates Script
         }
             else {
@@ -105,7 +104,7 @@ function Training-connect-RDC { # Connects upto 4 VMs based of resource group na
                 mstsc /v:$AZVMIP2 #Starts RDC connection to next listed server
             }
         if (!$AZRG3) { # Check for additional parameters
-            Write-Host "No Additional Prarameters Provided, Ending Script"
+            Write-Host "No Additional Parameters Provided, Ending Script"
             Break # Terminates Script
             }
             else {
@@ -118,7 +117,7 @@ function Training-connect-RDC { # Connects upto 4 VMs based of resource group na
                 mstsc /v:$AZVMIP3 #Starts RDC connection to next listed server
             }
         if (!$AZRG4) { # Check for additional parameters
-                Write-Host "No Additional Prarameters Provided, Ending Script"
+                Write-Host "No Additional Parameters Provided, Ending Script"
                 Break # Terminates Script
             }
                 else {
@@ -175,7 +174,7 @@ function Training-Remove-RGs { # Removes listed resource groups (Provided the gr
             }
     }
 }
-function Training-Deploy-SecretKey { # Deploys a new secret and value to listed Azure Keyvault
+function Training-Deploy-Secret { # Deploys a new secret and value to listed Azure Keyvault
     [CmdletBinding()]
     param 
     (
@@ -213,7 +212,7 @@ function Training-Deploy-ResourceLock {
         $AZRG1Name = (Get-AzResourceGroup -Name $AZRG1).ResourceGroupName # Associates user input to a resource group
         New-AzResourceLock -LockLevel $LockLevel -LockNotes "This a predefined lock for training" -LockName $AZRG1" Lock" -ResourceGroupName $AZRG1Name -Force # Deploys resource lock to resource group
         if (!$AZRG2) { # Check for additional parameters
-            Write-Host "No Additional Prarameters Provided, Ending Script"
+            Write-Host "No Additional Parameters Provided, Ending Script"
             Break # Terminates Script
         }
             else {
@@ -221,7 +220,7 @@ function Training-Deploy-ResourceLock {
                 New-AzResourceLock -LockLevel $LockLevel -LockNotes "This a predefined lock for training" -LockName $AZRG2" Lock" -ResourceGroupName $AZRG2Name -Force # Deploys resource lock to resource group
             }
         if (!$AZRG3) { # Check for additional parameters
-            Write-Host "No Additional Prarameters Provided, Ending Script"
+            Write-Host "No Additional Parameters Provided, Ending Script"
             Break # Terminates Script
             }
             else {
@@ -229,12 +228,129 @@ function Training-Deploy-ResourceLock {
                 New-AzResourceLock -LockLevel $LockLevel -LockNotes "This a predefined lock for training" -LockName $AZRG3" Lock" -ResourceGroupName $AZRG3Name -Force # Deploys resource lock to resource group
             }
         if (!$AZRG4) { # Check for additional parameters
-                Write-Host "No Additional Prarameters Provided, Ending Script"
+                Write-Host "No Additional Parameters Provided, Ending Script"
                 Break # Terminates Script
             }
             else {
                 $AZRG4Name = (Get-AzResourceGroup -Name $AZRG4).ResourceGroupName # Associates user input to a resource group
                 New-AzResourceLock -LockLevel $LockLevel -LockNotes "This a predefined lock for training" -LockName $AZRG4" Lock" -ResourceGroupName $AZRG4Name -Force # Deploys resource lock to resource group
             }  
+    }
+}
+function Training-Remove-ResourceLock { # This code needs a check against a lock not existing against a parameter
+[CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+            [string] $AZRG1,
+        [Parameter(Mandatory=$false, Position=1)]
+            [string] $AZRG2,
+        [Parameter(Mandatory=$false, Position=2)]
+            [string] $AZRG3,
+        [Parameter(Mandatory=$false, Position=3)]
+            [string] $AZRG4
+    )
+    Begin
+    {
+        $AZRG1Name = (Get-AzResourceGroup -Name $AZRG1).ResourceGroupName # Associates user input to a resource group
+        $lock1 = (Get-AzResourceLock -ResourceGroupName $AZRG1Name).resourceid # Pulls the resourceID for the resource lock from the listed resource group
+        Remove-AzResourceLock -ResourceId $lock1 -force # Removes the resource lock from the group
+        Write-Host $AZRG1Name"'s lock has been removed"
+        if (!$AZRG2) { # Check for additional parameters
+            Write-Host "No Additional Parameters Provided, Ending Script"
+            Break # Terminates Script
+        }
+            else {
+                $AZRG2Name = (Get-AzResourceGroup -Name $AZRG2).ResourceGroupName # Associates user input to a resource group
+                $lock2 = (Get-AzResourceLock -ResourceGroupName $AZRG2Name).resourceid # Pulls the resourceID for the resource lock from the listed resource group
+                Remove-AzResourceLock -ResourceId $lock2 -force # Removes the resource lock from the group
+                Write-Host $AZRG2Name"'s lock has been removed"
+            }
+        if (!$AZRG3) { # Check for additional parameters
+            Write-Host "No Additional Parameters Provided, Ending Script"
+            Break # Terminates Script
+        }
+            else {
+                $AZRG3Name = (Get-AzResourceGroup -Name $AZRG3).ResourceGroupName # Associates user input to a resource group
+                $lock3 = (Get-AzResourceLock -ResourceGroupName $AZRG3Name).resourceid # Pulls the resourceID for the resource lock from the listed resource group
+                Remove-AzResourceLock -ResourceId $lock3 -force # Removes the resource lock from the group
+                Write-Host $AZRG3Name"'s lock has been removed"
+            }
+        if (!$AZRG4) { # Check for additional parameters
+            Write-Host "No Additional Parameters Provided, Ending Script"
+            Break # Terminates Script
+        }
+            else {
+                $AZRG4Name = (Get-AzResourceGroup -Name $AZRG4).ResourceGroupName # Associates user input to a resource group
+                $lock4 = (Get-AzResourceLock -ResourceGroupName $AZRG4Name).resourceid # Pulls the resourceID for the resource lock from the listed resource group
+                Remove-AzResourceLock -ResourceId $lock4 -force # Removes the resource lock from the group
+                Write-Host $AZRG4Name"'s lock has been removed"
+            } 
+    }
+}
+function Training-GetHelp { # This is a local help file to assist the operator on the use and required changes to this script to ensure functionality
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+            [string] $FunctionName
+    )
+    Begin
+    {
+        if ($FunctionName -like "Training-Deploy-W*") {
+            Write-Host "**************************************************************************************************************************************************"
+            Write-Host "The syntax of this command is 'Training-Deploy-WinVM RG1NAME RG2NAME RG3NAME RG4NAME'"
+            Write-Host "This command deploys up to 4 resource group deployments"
+            Write-Host "The script should be updated with the operators prefered Templates and Parameters files on github"
+            Write-Host "In order for the Training-Connect-RDC function to work, the Parameters file needs to include the same Admin Username and the keyvault secret will need to match the VM Name"
+            Write-Host "If the operator needs to run Templates or Parameters files locally, the commands themselves will need to be changed from URI to file"
+            Write-Host "Ref: https://docs.microsoft.com/en-us/powershell/module/az.resources/new-azdeployment?view=azps-5.0.0#example-1--use-a-custom-template-and-parameter-file-to-create-a-deployment"    
+            Write-Host "**************************************************************************************************************************************************"
+        }
+        elseif ($FunctionName -like "Training-c*") {
+            Write-Host "**************************************************************************************************************************************************"
+            Write-Host "The syntax of this command is 'Training-Connect-RDC RG1NAME RG2NAME RG3NAME RG4NAME'"
+            Write-Host "This command will need to updated to the operators keyvault variables"
+            Write-Host "In addition, the operator will need to update the Admin user account name to match the parameters file"
+            Write-Host "This command is stuctured to pull the Secret from a keyvault using the VM name as the Secret name, any changes to this schema will need to be manually changed"
+            Write-Host "The order of connection is based off the order of the operaor input, this command uses the name of the RG to pull the needed info for the IP and VM name"
+            Write-Host "This command will not function for a RG with more than one public IP server"
+            Write-Host "**************************************************************************************************************************************************"
+        }
+        elseif ($FunctionName -like "Training-Deploy-R*") {
+            Write-Host "**************************************************************************************************************************************************"
+            Write-Host "The syntax of this command is 'Training-Deploy-ResourceLock R(or C) RG1NAME RG2NAME RG3NAME RG4NAME'"
+            Write-Host "No edits are needed for this command to run"
+            Write-Host "Will only apply a CanNotDelete or ReadOnly lock to a listed resource group"
+            Write-Host "The letters 'C' and 'R' can be used inleiu of CanNotDelete and ReadOnly, There is no correction done in this script for this"
+            Write-Host "The operator should update the hard coded message of the lock to match their schema"
+            Write-Host "**************************************************************************************************************************************************"
+        }
+        elseif ($FunctionName -like "Training-Deploy-Secret"){
+            Write-Host "**************************************************************************************************************************************************"
+            Write-Host "The syntax of this command is 'Training-Deploy-Secret SECRETVALUE SECRETNAME"
+            Write-Host "This funtion has hardcoded references to a keyvault that will need to changed by the operator to function"
+            Write-Host "If a secret name already exists, the secret value of this command will override the existing value, no prompt will be provided"
+            Write-Host "MS has noted that changes to the KeyVault will occur soon that may break functionality"
+            Write-Host "**************************************************************************************************************************************************"
+        }
+        elseif ($FunctionName -like "Training-D*") {
+            Write-Host "**************************************************************************************************************************************************"
+            Write-Host "The Training-Deploy Commands are used for deploying Assets, locks, and Keys"
+            Write-Host "Training-Deploy-WinVM is used for deploying servers from Github Templates and Parameters"
+            Write-Host "Training-Deploy-ResourceLock is used to deploy CanNotDelete and Read only locks to a (Up to 4) resource group(s)"
+            Write-Host "Training-Deploy-Secret is used to deploy a new secret to the keyvault within the command"
+            Write-Host "**************************************************************************************************************************************************"
+        }
+        else {
+            Write-Host "**************************************************************************************************************************************************"
+            Write-Host "The Functions in this script are"
+            Write-Host "Training-Deploy-WinVM"
+            Write-Host "Training-Deploy-ResourceLock"
+            Write-Host "Training-Deploy-Secret"
+            Write-Host "Training-connect-RDC"
+            Write-Host "Training-Remove-ResourceLock"
+            Write-Host "Training-Remove-RGs"
+            Write-Host "**************************************************************************************************************************************************"
+            
+        }
     }
 }
